a2612a73480bb8cfe17b5398abfb0b12f7b24e86
JohnWolf12 <82612279+JohnWolf12@users.noreply.github.com>
2023-06-12 00:17:40 +0200

    Tworzenieogloszen

--- aplikacjaogloszeniowa/urls.py
+++ aplikacjaogloszeniowa/urls.py
@@ -1,3 +1,8 @@
-urlpatterns = [
+from django.urls import path
 
+from aplikacjaogloszeniowa import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('dodanieogloszenia/', views.addAnnouncement_view, name="dodanieOgloszenia"),
 ]
