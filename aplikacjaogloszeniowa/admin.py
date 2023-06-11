916f056a60cd438f4cd78c0da9b3c7ee25b1cd13
JohnWolf12 <82612279+JohnWolf12@users.noreply.github.com>
2023-06-11 23:29:56 +0200

    UtworzenieModeluDanych (#4)

--- aplikacjaogloszeniowa/admin.py
+++ aplikacjaogloszeniowa/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from aplikacjaogloszeniowa.models import Kategoria, Ogloszenie
+
+admin.site.register(Kategoria)
+admin.site.register(Ogloszenie)
--- aplikacjaogloszeniowa/models.py
+++ aplikacjaogloszeniowa/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Kategoria(models.Model):
+    nazwa = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nazwa
+
+    class Meta:
+        verbose_name = "kategoria"
+        verbose_name_plural = "kategorie"
+
+
+class Ogloszenie(models.Model):
+    nazwa = models.CharField(max_length=100)
+    cena = models.DecimalField(max_digits=9, decimal_places=2)
+    opis = models.TextField(blank=True, max_length=1000)
+    kategoria = models.ForeignKey(Kategoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nazwa
+
+    class Meta:
+        verbose_name = "ogłoszenie"
+        verbose_name_plural = "ogłoszenia"
